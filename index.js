d735974bd2e90efc2dd332a5cf2abfddd16901cc
Code Hz <CodeHz@outlook.com>
2016-03-22 20:32:17 +0800

    add submit button
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ function getlen(text) {
   return (text.length + encodeURI(text).split(/%..|./).length - 1) / 2;
 }
 //检查函数，并设置标记
-function check(el, box, tip, template) {
-  if (template.validator(el.value)) {
+function check(value, box, tip, template) {
+  if (template.validator(value)) {
     box.className = "box verified";
     tip.setAttribute("data-validation", template.success);
+    return true;
   } else {
     box.className = "box error";
     tip.setAttribute("data-validation", template.fail);
+    return false;
   }
 }
 //生成
@@ -27,9 +29,13 @@ function genForm() {
     tip.setAttribute("data-info", template.rules);
     tip.className = "tip";
 
+    let valueGetter = null;//保存用于无须遍历DOM树快速查找含有值的元素
+
     if (template.type == 'radio') {
       let container = document.createElement("div");//单选的父元素
       container.className = 'radio-option';
+
+      let elCache = [];
 
       for (let option in template.radioExt) {
         let labelContainer = document.createElement("label");//每个选项的标签
@@ -38,38 +44,68 @@ function genForm() {
         optionEl.setAttribute("type", "radio");
         optionEl.setAttribute("name", template.name);
         optionEl.setAttribute("value", option);
-        optionEl.addEventListener("change", () => check(optionEl, box, tip, template));//设置验证器
+        optionEl.addEventListener("change", () => check(optionEl.value, box, tip, template));//设置验证器
+
+        elCache.push(optionEl);
 
         labelContainer.appendChild(optionEl);
         labelContainer.appendChild(document.createTextNode(template.radioExt[option]));
 
         container.appendChild(labelContainer);
       }
+
+      //catch elCache to lambda function
+      valueGetter = () => {
+        for (let it of elCache) if (it.checked) return it.value;
+        return null;
+      };
+
       box.appendChild(container);
     } else {
       let el = document.createElement("input");//文本框
       el.setAttribute("type", template.type);
       el.setAttribute("name", template.name);
+
       if (template.attr) {
         for (let key in template.attr) {
           el.setAttribute(key, template.attr[key]);
         }
       }
-      el.addEventListener('blur', () => check(el, box, tip, template));//设置验证器
+
+      el.addEventListener('blur', () => check(el.value, box, tip, template));//设置验证器
+
+      valueGetter = () => el.value;
+
       box.appendChild(el);
     }
 
     box.appendChild(tip);
 
-    return box;
+    let validator = () => check(valueGetter(), box, tip, template);
+
+    return {validator, box};
   });
 }
 
 let formels = genForm();
 
+function checkAll() {
+  for (let formel of formels)
+    if (!formel.validator())
+      return alert('提交失败');
+  return alert('提交成功');
+}
+
 window.onload = () => {
   let form = document.getElementById('target');
-  for (let el in formels) {
-    form.appendChild(formels[el]);//逐个加入
-  }
+
+  for (let formel of formels)
+    form.appendChild(formel.box);//逐个加入
+
+  let el = document.createElement("input");//加入按钮
+  el.setAttribute("type", "button");
+  el.value = "Submit";
+  el.addEventListener('click', checkAll);
+  console.log(el);
+  form.appendChild(el);
 }
